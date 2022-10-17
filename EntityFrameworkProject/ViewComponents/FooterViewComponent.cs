c1d479525ff6b59eb57571dea345ecc3a87445ed
Nara19941008 <narminim@code.edu.az>
2022-10-17 21:50:14 +0300

    Added bio table

---
+++ EntityFrameworkProject/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public FooterViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Footer.ToListAsync());
+        }
+
+    }
+    
+}
